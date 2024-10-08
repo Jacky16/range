671585aa03d425410049fe195f66aac2aafa0b5c
Adria Martinez <adriamartinez94@gmail.com>
2024-10-08 18:45:25 +0200

    Initial commit from Create Next App

---
+++ src/app/exercise1/page.tsx
@@ -0,0 +1,9 @@
+const PageExerciseOne = () => {
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <h1 className="text-4xl font-bold">Exercise 1</h1>
+    </div>
+  );
+};
+
+export default PageExerciseOne;
