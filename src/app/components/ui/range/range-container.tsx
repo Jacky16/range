8929fa346823266b2867718b1556269819d68d5b
Adria Martinez <adriamartinez94@gmail.com>
2024-10-12 21:37:22 +0200

    feat: Implement snap values functionality on range container

--- src/app/components/ui/range/range-container.tsx
+++ src/app/components/ui/range/range-container.tsx
@@ -3,7 +3,11 @@ import { useRange } from "./contexts/range-context";
 import InputRange from "./input-range";
 import Thumb from "./thumb";
 import { Track, TrackRange } from "./track";
-import { calculateThumbPosition } from "./utils/utils";
+import {
+  calculateThumbPosition,
+  findClosestValue,
+  getClosestValueIndex,
+} from "./utils/utils";
 
 const RangeContainer = () => {
   const {
@@ -13,9 +17,11 @@ const RangeContainer = () => {
     initialValue,
     thumbSize,
     onValueCommit,
+    values,
   } = useRange();
 
   const trackRef = useRef<HTMLDivElement>(null);
+
   const leftThumbRef = useRef<HTMLDivElement>(null);
   const rightThumbRef = useRef<HTMLDivElement>(null);
 
@@ -28,7 +34,6 @@ const RangeContainer = () => {
     if (!trackRef.current) return;
 
     const mouseXPosition = event.clientX;
-
     const trackRect = trackRef.current.getBoundingClientRect();
 
     let thumbPosition = calculateThumbPosition(
@@ -40,46 +45,47 @@ const RangeContainer = () => {
       thumbSize / 2
     );
 
+    let value =
+      values.length > 0
+        ? findClosestValue(thumbPosition, values)
+        : thumbPosition;
+
+    const leftValueIndex = getClosestValueIndex(values, rangeValue.min);
+    const rightValueIndex = getClosestValueIndex(values, rangeValue.max);
+
+    const differenceIndexFromThumbs = rightValueIndex - leftValueIndex;
+
     if (isDraggingLeft.current) {
-      if (thumbPosition <= initialValue.min) {
-        thumbPosition = initialValue.min;
-      } else if (thumbPosition >= rangeValue.max) {
-        thumbPosition = rangeValue.max;
+      if (value <= initialValue.min) {
+        value = initialValue.min;
+      } else if (
+        value >= rangeValue.max ||
+        (differenceIndexFromThumbs <= 1 && values.length > 0)
+      ) {
+        return;
       }
 
-      updateRangeValue({
-        ...rangeValue,
-        min: thumbPosition,
-      });
-
-      rangeValueRef.current = {
-        ...rangeValue,
-        min: thumbPosition,
-      };
+      updateRangeValue({ ...rangeValue, min: value });
+      rangeValueRef.current = { ...rangeValue, min: value };
     }
 
     if (isDraggingRight.current) {
-      if (thumbPosition >= initialValue.max) {
-        thumbPosition = initialValue.max;
-      } else if (thumbPosition <= rangeValue.min) {
-        thumbPosition = rangeValue.min - 0.01;
+      if (value >= initialValue.max) {
+        value = initialValue.max;
+      } else if (
+        value <= rangeValue.min ||
+        (differenceIndexFromThumbs <= 1 && values.length > 0)
+      ) {
+        return;
       }
 
-      updateRangeValue({
-        ...rangeValue,
-        max: thumbPosition,
-      });
-
-      rangeValueRef.current = {
-        ...rangeValue,
-        max: thumbPosition,
-      };
+      updateRangeValue({ ...rangeValue, max: value });
+      rangeValueRef.current = { ...rangeValue, max: value };
     }
   };
 
   const handleMouseDownLeft = () => {
     isDraggingLeft.current = true;
-
     updateIsDragging(true);
 
     window.addEventListener("mousemove", handleMouseMove);
@@ -88,7 +94,6 @@ const RangeContainer = () => {
 
   const handleMouseDownRight = () => {
     isDraggingRight.current = true;
-
     updateIsDragging(true);
 
     window.addEventListener("mousemove", handleMouseMove);
@@ -111,13 +116,11 @@ const RangeContainer = () => {
       <InputRange position="left" />
       <Track ref={trackRef}>
         <TrackRange />
-
         <Thumb
           position="left"
           ref={leftThumbRef}
           onMouseDown={handleMouseDownLeft}
         />
-
         <Thumb
           position="right"
           ref={rightThumbRef}
